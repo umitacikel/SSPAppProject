fd44db359e03577aa00f01dea2c578d88df6038b
Yunas09 <yunas3000@hotmail.com>
2017-10-02 13:04:38 +0200

    SSP Call Virker. SSP Email Form/men sender ikke emails endu Sat Logo ind

--- app/src/main/java/com/example/yunas/sspappproject/SSP_Fragment_Help.java
+++ app/src/main/java/com/example/yunas/sspappproject/SSP_Fragment_Help.java
@@ -1,8 +1,12 @@
 package com.example.yunas.sspappproject;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +16,7 @@ import android.widget.Button;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SSP_Fragment_Help extends Fragment {
+public class SSP_Fragment_Help extends Fragment implements View.OnClickListener {
 
 
     public SSP_Fragment_Help() {
@@ -25,27 +29,41 @@ public class SSP_Fragment_Help extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_ssp_help, container, false);
+
+
         View btnCall = (Button) v.findViewById(R.id.Btn_Call);
+        btnCall.setOnClickListener((View.OnClickListener) this);
 
-
-
-
-/*
-    private void call() {
-
-        Intent call = new Intent(Intent.ACTION_DIAL);
-        call.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        call.setData(Uri.parse("tel:" + "+25512492"));
-        startActivity(call);
-    }
-*/
-
-
-
-
-
-
+        View btnEmail = (Button) v.findViewById(R.id.Btn_Email);
+        btnEmail.setOnClickListener((View.OnClickListener) this);
         return v;
     }
-}
+
+
+        public void onClick(View v) {
+            if(v.getId() == R.id.Btn_Call)
+            {
+                Intent call = new Intent(Intent.ACTION_DIAL);
+                call.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                call.setData(Uri.parse("tel:" + "+25512492"));
+                startActivity(call);
+            }
+            else if (v.getId() == R.id.Btn_Email)
+            {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                SSP_Fragment_Help_Email NAME = new SSP_Fragment_Help_Email();
+                fragmentTransaction.replace(R.id.fragment, NAME);
+                fragmentTransaction.commit();
+            }
+
+        }
+
+
+
+
+
+    }
+
+
 
--- app/src/main/java/com/example/yunas/sspappproject/SSP_Fragment_Help_Email.java
+++ app/src/main/java/com/example/yunas/sspappproject/SSP_Fragment_Help_Email.java
@@ -1,17 +1,23 @@
 package com.example.yunas.sspappproject;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import static android.R.id.message;
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SSP_Fragment_Help_Email extends Fragment {
+public class SSP_Fragment_Help_Email extends Fragment  implements View.OnClickListener {
 
 
     public SSP_Fragment_Help_Email() {
@@ -23,7 +29,25 @@ public class SSP_Fragment_Help_Email extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ssp_help_email, container, false);
+        View v = inflater.inflate(R.layout.fragment_ssp_help, container, false);
+
+
+
+        View btnSendEmail = (Button) v.findViewById(R.id.Btn_SendEmail);
+        btnSendEmail.setOnClickListener((View.OnClickListener) this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.Btn_SendEmail)
+        {
+            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto","yunas3000@gmail.com", null));
+
+            intent.putExtra(Intent.EXTRA_TEXT, message);
+            startActivity(Intent.createChooser(intent, "GMAIL"));
+        }
     }
 
 }
