230b066a2ab09aed96353e347a631686de6af6d8
Yunas09 <yunas3000@hotmail.com>
2017-10-03 12:50:11 +0200

    SSP Email Virker nu også. Husk Ændrer Mail til SSP

---
+++ app/src/main/java/com/example/yunas/sspappproject/Config.java
@@ -0,0 +1,10 @@
+package com.example.yunas.sspappproject;
+
+/**
+ * Created by Yunas on 03-10-2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="yunas3000@gmail.com";
+    public static final String PASSWORD ="Yunasyunas09";
+}
--- app/src/main/java/com/example/yunas/sspappproject/SSP_Fragment_Help_Email.java
+++ app/src/main/java/com/example/yunas/sspappproject/SSP_Fragment_Help_Email.java
@@ -1,53 +1,64 @@
 package com.example.yunas.sspappproject;
 
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-import static android.R.id.message;
-
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SSP_Fragment_Help_Email extends Fragment  implements View.OnClickListener {
+public class SSP_Fragment_Help_Email extends Fragment implements View.OnClickListener   {
 
 
     public SSP_Fragment_Help_Email() {
         // Required empty public constructor
     }
 
+    String Email = "umitacike@live.dk";
+  //  EditText editTextEmail;
+    EditText editTextSubject;
+    EditText editTextMessage;
+
+    Button buttonSend;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_ssp_help, container, false);
+        View v = inflater.inflate(R.layout.fragment_ssp_help_email, container, false);
+    //    editTextEmail = (EditText) v.findViewById(R.id.editTextEmail);
+        editTextSubject = (EditText) v.findViewById(R.id.editTextSubject);
+        editTextMessage = (EditText) v.findViewById(R.id.editTextMessage);
 
 
+        buttonSend = (Button) v.findViewById(R.id.buttonSend);
 
-        View btnSendEmail = (Button) v.findViewById(R.id.Btn_SendEmail);
-        btnSendEmail.setOnClickListener((View.OnClickListener) this);
+        buttonSend.setOnClickListener(this);
+
         return v;
     }
 
+    private void sendEmail() {
+        //Getting content for email
+      //  String email = editTextEmail.getText().toString().trim();
+        String subject = editTextSubject.getText().toString().trim();
+        String message = editTextMessage.getText().toString().trim();
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(getActivity(), Email, subject, message);
+
+        //Executing sendmail to send email
+        sm.execute();
+    }
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.Btn_SendEmail)
-        {
-            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                    "mailto","yunas3000@gmail.com", null));
+        sendEmail();
 
-            intent.putExtra(Intent.EXTRA_TEXT, message);
-            startActivity(Intent.createChooser(intent, "GMAIL"));
-        }
     }
-
 }
